7dda30412434b0398d4317d6eebebfa48233f8bb
Mike Mooc <moocmaster21@tutanota.com>
2022-01-20 23:39:03 -0600

    basic ship and controls

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (0,0,0)
+
+        #  Ship settings
+        self.ship_speed = 1.5 
