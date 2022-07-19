a090c3b70769d08283d2877f7fb52bd1ccfdfe09
Mike Mooc <moocmaster21@tutanota.com>
2022-07-18 22:16:30 -0500

    refactorer gunner.py

--- src/gunner.py
+++ src/gunner.py
@@ -1,7 +1,5 @@
 import pygame
 from pygame.sprite import Sprite
-from math import sqrt
-from random import randint
 from gunner_bullet import GunnerBullet
 
 
@@ -20,12 +18,12 @@ class Gunner(Sprite):
         self.radius = 50
 
     def _set_assets(self, ai_game):
-        """Loads the audio and images for the gunner and sets their properties."""
+        """Load the audio and images for the gunner and sets their properties."""
         self.image = pygame.image.load('assets/images/alien.bmp')
         self.fire_sfx = ai_game.sound.gunner_sfx
 
     def _make_game_objects(self, ai_game):
-        """Makes the objects necessary for the gunner to function."""
+        """Make the objects necessary for the gunner to function."""
         self.game = ai_game
         self.screen = ai_game.screen
         self.ship = ai_game.ship
@@ -33,21 +31,21 @@ class Gunner(Sprite):
         self.gunner_bullets = pygame.sprite.Group()
 
     def _set_gunner_stats(self):
-        """Sets the combat stats for the gunner."""
+        """Set the combat stats for the gunner."""
         self.hitpoints = 9
         self.berserk = False
         self.fire_delay = 1000
         self.last_shot = pygame.time.get_ticks()
 
     def _set_initial_coordinates(self):
-        """Sets the initial coordinates for gunners and their rects."""
+        """Set the initial coordinates for gunners and their rects."""
         self.rect.x = self.screen_rect.right
         self.rect.y = self.screen_rect.centery
         self.y = float(self.rect.y)
         self.x = float(self.rect.x)
 
     def update(self, dt):
-        """Update method for mines"""
+        """Update method for mines."""
         self._move_gunner(dt)
         self._fire_bullets()
         self._show_damage()
@@ -55,7 +53,7 @@ class Gunner(Sprite):
             self.gunner_bullets.update(dt)
 
     def _fire_bullets(self):
-        """Creates new bullets based on a cooldown"""
+        """Create new bullets based on a cooldown."""
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.fire_delay:
             self.last_shot = now
@@ -64,7 +62,7 @@ class Gunner(Sprite):
             self.game.sound.play_sfx("gunner")
 
     def _move_gunner(self, dt):
-        """Updates the position of the mines."""
+        """Update the position of the mines."""
         speed_bonus = 1.25 if self.berserk else 1
         self.fire_delay = 800 if self.berserk else 1000
         if self.x > self.screen_rect.right - 175:
@@ -77,7 +75,7 @@ class Gunner(Sprite):
         self.rect.x = self.x
 
     def _show_damage(self):
-        """Updates the image of the gunner if it's low on HP."""
+        """Update the image of the gunner if it's low on HP."""
         if self.hitpoints < 5:
             self.image.fill((218, 44, 67), special_flags=pygame.BLEND_MIN)
             self.berserk = True
